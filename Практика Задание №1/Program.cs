34efaa9d223c07a4615130f352ce1a87696f0a66
Marat <valeevmaratf@gmail.com>
2017-07-31 15:15:58 +0500

    Выровнил комментарии

--- Практика Задание №1/Program.cs
+++ Практика Задание №1/Program.cs
@@ -20,21 +20,20 @@ namespace Практика_Задание__1
 
         public static void DoLineCross(string[] coords)
         {                            // Получает координаты
-            long x1 = int.Parse(coords[0]), x2 = int.Parse(coords[2]), x3 = int.Parse(coords[4]), x4 = int.Parse(coords[6]), // Координаты х и у
-                y1 = int.Parse(coords[1]), y2 = int.Parse(coords[3]), y3 = int.Parse(coords[5]), y4 = int.Parse(coords[7]);
+            long x1 = long.Parse(coords[0]), x2 = long.Parse(coords[2]), x3 = long.Parse(coords[4]), x4 = long.Parse(coords[6]),                    // Координаты х и у
+                y1 = long.Parse(coords[1]), y2 = long.Parse(coords[3]), y3 = long.Parse(coords[5]), y4 = long.Parse(coords[7]);
 
             bool pseudoscalarProduct1 = ((x2 - x1) * (y4 - y1) - (x4 - x1) * (y2 - y1)) * ((x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1)) <= 0, // Произведение косых произведений: [P1P2, P1P4] * [P1P2, P1P3], P1-P4, имеют координаты с соответсвующими индексами, требуется для того, чтобы проверить, что концы каждого из отрезков лежат по разные стороны относительного концов другого отрезка
                  pseudoscalarProduct2 = ((x4 - x3) * (y1 - y3) - (x1 - x3) * (y4 - y3)) * ((x4 - x3) * (y2 - y3) - (x2 - x3) * (y4 - y3)) <= 0, // Произведение косых произведений: [P3P4, P3P1] * [P3P4, P3P2], P1-P4, имеют координаты с соответсвующими индексами, требуется для того, чтобы проверить, что концы каждого из отрезков лежат по разные стороны относительного концов другого отрезка
-                 scalarProduct1 = (x1 - x3) * (x2 - x3) + (y1 - y3) * (y2 - y3) <= 0, // Скалярное произведение (P3P1,P3P2)
-                 scalarProduct2 = (x1 - x4) * (x2 - x4) + (y1 - y4) * (y2 - y4) <= 0, // Скалярное произведение (P4P1,P4P2)
-                 scalarProduct3 = (x3 - x1) * (x4 - x1) + (y3 - y1) * (y4 - y1) <= 0, // Скалярное произведение (P1P3,P1P4)
-                 scalarProduct4 = (x3 - x2) * (x4 - x2) + (y3 - y2) * (y4 - y2) <= 0; // Скалярное произведение (P2P3,P2P4)
+                 scalarProduct1 = (x1 - x3) * (x2 - x3) + (y1 - y3) * (y2 - y3) <= 0,                                                           // Скалярное произведение (P3P1,P3P2)
+                 scalarProduct2 = (x1 - x4) * (x2 - x4) + (y1 - y4) * (y2 - y4) <= 0,                                                           // Скалярное произведение (P4P1,P4P2)
+                 scalarProduct3 = (x3 - x1) * (x4 - x1) + (y3 - y1) * (y4 - y1) <= 0,                                                           // Скалярное произведение (P1P3,P1P4)
+                 scalarProduct4 = (x3 - x2) * (x4 - x2) + (y3 - y2) * (y4 - y2) <= 0;                                                           // Скалярное произведение (P2P3,P2P4)
 
-            if (pseudoscalarProduct1 & pseudoscalarProduct2 & // Если оба произведения косых произведений <=0 значит концы каждого из отрезков лежат по разные стороны относительного концов другого отрезка 
-                (scalarProduct1 | scalarProduct2 | scalarProduct3 | scalarProduct4)) // Но, требуется проверить, что хотя бы один из концов одного отрезка принадлежит другому отрезку, в случае если отрезки части одной прямой (произведение косых произведений=0)
-                Console.WriteLine("Yes"); // Записывает "Yes", если общая точка есть
-
-            Console.WriteLine("No"); // Или "No" - в противном случае
+            if (pseudoscalarProduct1 && pseudoscalarProduct2 &&                                                                                 // Если оба произведения косых произведений <=0 значит концы каждого из отрезков лежат по разные стороны относительного концов другого отрезка 
+                (scalarProduct1 || scalarProduct2 || scalarProduct3 || scalarProduct4))                                                         // Но, требуется проверить, что хотя бы один из концов одного отрезка принадлежит другому отрезку, в случае если отрезки части одной прямой (произведение косых произведений=0)
+                Console.WriteLine("Yes");                                                                                                       // Записывает "Yes", если общая точка есть
+            else Console.WriteLine("No");                                                                                                       // Или "No" - в противном случае
         }                                           // Выясняет пересекаются ли отрезки с помощью угловых коэффициентов
 
         public static void Main(string[] args)
